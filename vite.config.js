98e14aee351c485c0f4f0b0077a2fc57f3d68a2d
fortuneller <vcankat14@ku.edu.tr>
2025-02-05 01:48:47 -0500

    Initial commit: Setup project for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/MyPortfolio/", // This ensures correct asset paths for GitHub Pages
+});
